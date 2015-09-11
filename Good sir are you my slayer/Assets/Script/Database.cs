8f7fcece295325e3ee9a8a396312e7cc510853e3
unknown <1231286@5B1-06.ad.dawsoncollege.qc.ca>
2015-09-11 18:41:56 -0400

    Should work now

---
+++ Good sir are you my slayer/Assets/Script/Database.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+namespace Database{
+
+	public class Get
+	{
+		public enum Type {Head, Body, Accessory}; 
+		static public string[] FirstName = new string[] {"Lol","JK","Hi","moo"};
+		static public string[] LastName = new string[] {"L.","K.","J,","H."};
+		static public string[] NeedName = new string[] {"Eat","Smoke","Bathroom","Drunkness"};
+		static public string[] Consumable = new string[] {"Poison","Snack","Drink","Bandage","Firecracker","PainKiller"};
+		static public string Name{get{return FirstName [Random.Range (0, FirstName.Length)]+" "+LastName [Random.Range (0, LastName.Length)];}}
+	}
+
+	public class Need
+	{
+		public string Name;
+		public int Meter = 100;
+	}
+
+	public class Cast
+	{
+		static public void Consumable(Item item, Player player)
+		{
+			if (item.Name == "Drink") Drink(item,player);
+			else if (item.Name == "Snack") Snack(item,player);
+			else if (item.Name == "Bandage") Bandage(player);
+		}
+
+		public class Spawn
+		{
+
+		}
+		
+		public class Weapon
+		{
+			
+		}
+
+		static public void Drink(Item item, Player player)
+		{
+			player.Needs [4].Meter += item.Amount;
+		}
+
+		static public void Snack(Item item, Player player)
+		{
+			player.Needs [1].Meter += item.Amount;
+		}
+		static public void Bandage(Player player)
+		{
+			if (player.IsBleeding) player.IsBleeding = false;
+		}
+
+	}
+}
---
+++ Good sir are you my slayer/Assets/Script/Item.cs
@@ -0,0 +1,80 @@
+﻿using UnityEngine;
+using System.Collections;
+using Database;
+
+public class Item : MonoBehaviour {
+
+	public enum type {Consumable,Spawn,Weapon};
+	public enum consumable{Any, Poison, Snack, Drink, Bandage, Firecracker, PainKiller};
+	public bool IsPoisoned, Drawn, Lethal;
+	public string Name;
+	public Collider Range, Notice;
+	public type Type; 
+	public consumable IsConsumable;
+	public GameObject Loot;
+	public int Ammo, Amount, Bleed, Force, Suspicion, ThrowAmount;
+	public float DrawSpeed, AttackSpeed;
+
+	void Awake()
+	{
+		if (Type == type.Consumable)
+		{
+			if (IsConsumable != consumable.Any) Name = IsConsumable.ToString();
+			else Name = Get.Consumable[Random.Range(0,Get.Consumable.Length)];
+		}
+	}
+
+	//Check if Player is picking up or poisoning the loot
+	void OnColliderEnter(Collider col)
+	{
+		if (Type == type.Spawn) 
+		{
+			Player player = col.GetComponent<Player> ();
+			if (Input.GetKeyDown (KeyCode.C))
+			{
+				/*if (player.Slots[3] != null)
+					if (player.Slots[3].IsConsumable == consumable.Poison && IsConsumable == consumable.Drink && IsPoisoned == false) 
+				{
+					IsPoisoned = true;
+					player.Slots[3] = null;
+				}*/
+			} else if (player != null) 
+			{
+				if (Input.GetKeyDown (KeyCode.V)) GiveLoot (player);
+			}
+		}
+	}
+	// Update is called once per frame
+	void Update () 
+	{
+	}
+	void GiveLoot(Player player)
+	{
+		Item loot = Loot.GetComponent<Item>();
+		if (loot.Type == type.Consumable) 
+		{
+			if (player.Slots [3] == null)
+			{
+				if (IsPoisoned == true && loot.Name == "Drink") GivePoison();
+				player.Slots[3] = Loot;
+			}
+		} else if (loot.Type == type.Weapon) GiveWeapon(player);
+	}
+	void CastItem(Player player)
+	{
+		if (Type == type.Consumable) Cast.Consumable(this, player);
+	}
+	
+	void GivePoison()
+	{
+		Item loot = Loot.GetComponent<Item>();
+		loot.IsPoisoned = true;
+		IsPoisoned = false;
+	}
+
+	void GiveWeapon(Player player)
+	{
+		if (player.Slots [1] == null) player.Slots [1] = Loot;
+		else if (player.Slots [2] == null) player.Slots [2] = Loot;
+	}
+}
