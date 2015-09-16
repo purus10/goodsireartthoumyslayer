ef8489877b72dcfc799fd9f1d90dd30d9d3e229e
unknown <1231286@5B1-06.ad.dawsoncollege.qc.ca>
2015-09-15 20:39:54 -0400

    no message

--- Good sir are you my slayer/Assets/Script/HUD_Inventory.cs
+++ Good sir are you my slayer/Assets/Script/HUD_Inventory.cs
@@ -7,6 +7,10 @@ public class HUD_Inventory : MonoBehaviour {
 	public Sprite[] Anim_Highlight;
 	public GameObject[] Icons;
 	public GameObject[] Inventory;
+	public float Anim_speed;
+	//static public bool ;
+	float time;
+	int frame;
 
 	void Awake()
 	{
@@ -28,11 +32,47 @@ public class HUD_Inventory : MonoBehaviour {
 			for (int i = 0; i < Inventory.Length;i++)
 			{
 				if(Get.player.Selected == Get.player.Slots[i])
+				{
 				transform.position = Inventory[i].transform.position;
+				}
 			}
 		}
+	}
 
+	void AnimateHighlight()
+	{
 
+		if (frame <= Anim_Highlight.Length-1)
+		{
+			time ++;
+			if (time >= Anim_speed)
+			{
+				GetComponent<SpriteRenderer>().sprite = Anim_Highlight[frame++];
+				time = 0;
+			}
+			if (frame == Anim_Highlight.Length-1)
+			{
+				if (frame != 0)
+				{
+					time ++;
+					if (time >= Anim_speed)
+					{
+						GetComponent<SpriteRenderer>().sprite = Anim_Highlight[frame--];
+						time = 0;
+					}
+				}
+			}
+		} else frame = 0;
 			
 	}
+
+	/*if (frame <= Walk_Up_Left.Length-1)
+	{
+		time ++;
+		if (time >= Anim_speed)
+		{
+			player.sprite = check[frame++];
+			time = 0;
+		}
+	} else frame = 0;*/
 }
