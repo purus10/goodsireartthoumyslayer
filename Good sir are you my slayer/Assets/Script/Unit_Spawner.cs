3f390d6447c4ba7e56c6ac863505d2a1e0a05891
unknown <1231286@5B1-06.ad.dawsoncollege.qc.ca>
2015-12-01 20:10:09 -0500

    Fixed some bugs.

--- Good sir are you my slayer/Assets/Script/Unit_Spawner.cs
+++ Good sir are you my slayer/Assets/Script/Unit_Spawner.cs
@@ -10,6 +10,7 @@ public class Unit_Spawner : NetworkBehaviour {
     [SerializeField] GameObject BUTLER_prefab;
     [SerializeField] GameObject CLUE_prefab;
     [SerializeField] GameObject[] Item_prefab;
+    [SerializeField] GameObject[] CONSUME_Prefab;
     public List<Vector3> SpawnPoints = new List<Vector3>();
     public List<Vector3> ItemSpawnPoints = new List<Vector3>();
     public GameObject startscreen;
@@ -21,6 +22,7 @@ public class Unit_Spawner : NetworkBehaviour {
     public int NumberOfButlers;
     public int NumberOfItems;
     public int NumberOfClues;
+    public int NumberOfConsumables;
     static public bool StartMatch;
 
     public void SpawnUnits()
@@ -64,6 +66,26 @@ public class Unit_Spawner : NetworkBehaviour {
                 ItemSpawnPoints.RemoveAt(position);
             }
         }
+
+        for (int i = 0; i < NumberOfItems; i++)
+        {
+            if (i < ItemSpawnPoints.Count)
+            {
+                int position = Random.Range(0, ItemSpawnPoints.Count - 1);
+                SpawnItem(ItemSpawnPoints[position]);
+                ItemSpawnPoints.RemoveAt(position);
+            }
+        }
+
+        for (int i = 0; i < NumberOfConsumables; i++)
+        {
+            if (i < ItemSpawnPoints.Count)
+            {
+                int position = Random.Range(0, ItemSpawnPoints.Count - 1);
+                SpawnCpnsumables(ItemSpawnPoints[position]);
+                ItemSpawnPoints.RemoveAt(position);
+            }
+        }
     }
 
     public override void OnStartServer()
@@ -91,12 +113,19 @@ public class Unit_Spawner : NetworkBehaviour {
 
     void SpawnItem(Vector3 position)
     {
-
+        GameObject go = GameObject.Instantiate(Item_prefab[Random.Range(0, Item_prefab.Length - 1)], position, Quaternion.identity) as GameObject;
+        NetworkServer.Spawn(go);
     }
 
     void SpawnClues(Vector3 position)
     {
         Instantiate(CLUE_prefab, position, Quaternion.identity);
+    }
+
+    void SpawnCpnsumables(Vector3 position)
+    {
+        GameObject go = GameObject.Instantiate(CONSUME_Prefab[Random.Range(0, CONSUME_Prefab.Length - 1)], position, Quaternion.identity) as GameObject;
+        NetworkServer.Spawn(go);
     }
 
     // Use this for initialization
